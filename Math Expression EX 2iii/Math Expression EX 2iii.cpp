a8ac8a2f31ae316d8793714f327f1e5787ea065a
mbashit <michaelbashitapwa52@gmail.com>
2022-08-10 02:19:06 +0300

    Add project files.

---
+++ Math Expression EX 2iii/Math Expression EX 2iii.cpp
@@ -0,0 +1,22 @@
+// Math Expression EX 2iii.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath.>
+#include<math.h>
+
+using  namespace std;
+
+int main()
+{
+	double x,n, y;
+	cout << "enter the value x";
+	cin >> x;
+	y = sqrt(pow(x, 4)-2 * pow(x, 3) -7* pow(x, 2) + x) / pow(n, 1 / 3) * (4*(7 * pow(x, 4)-3 / 4 * pow(x, 3)))(7 * pow(x,2)+x);
+	
+	cout << "the result is" << y << endl;
+
+	return 0;
+
+}
+
